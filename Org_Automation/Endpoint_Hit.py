9471e56afd1005fdea0099aff5d9bb683aeb3def
vamsykrishnak <kamasanivamsy@gmail.com>
2024-04-29 23:04:24 +0530

    y

--- Org_Automation/Endpoint_Hit.py
+++ Org_Automation/Endpoint_Hit.py
@@ -1,11 +1,11 @@
-import json
-import requests
-from socket import timeout
 import logging
 
+import requests
+
+
 def hit_endpoint(url):
-    list=[]
-    if(url!="null"):
+    list = []
+    if url != "null":
         data = requests.get(url)
         #print(data.json())
         dump = data.json()
@@ -13,8 +13,8 @@ def hit_endpoint(url):
         for link in dump["entries"]:
             print(link['Link'])
             try:
-                data2 = requests.get(link['Link'],timeout=10)
-                if (data2.status_code==200):
+                data2 = requests.get(link['Link'], timeout=10)
+                if data2.status_code == 200:
                     list.append(link['Link'])
                     print(list)
                 else:
@@ -24,5 +24,6 @@ def hit_endpoint(url):
 
     else:
         print("Error loading the url")
+
 
 hit_endpoint("https://api.publicapis.org/entries")
--- Python/QR_CODE_WORKING.py
+++ Python/QR_CODE_WORKING.py
@@ -1,14 +1,11 @@
-#pip install pyqrcode
+import pyqrcode
 
-import pyqrcode 
-from pyqrcode import QRCode 
-  
 # String which represent the QR code 
 s = "https://www.youtube.com/channel/UCBz4yaxNxfiz1XYh-07UfWQ"
-  
+
 # Generate QR code 
-url = pyqrcode.create(s) 
-  
+url = pyqrcode.create(s)
+
 # Create and save the png file naming "myqr.png" 
 #Scalable Vector Graphics
-url.svg("myyoutube.svg", scale = 8) 
+url.svg("YouTube.svg", scale=8)
